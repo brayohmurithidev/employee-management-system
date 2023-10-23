829643e9358c33f48cbd119faf4c421dcf3797e6
brayohmurithidev <murithibrianm@gmail.com>
2023-10-23 23:39:20 +0300

    initialize user controller

--- api/v1/controllers/users.controller.js
+++ api/v1/controllers/users.controller.js
@@ -1,4 +1,9 @@
 import { User } from "../models/index.models.js";
 import logger from "../utils/logger.js";
 
-export const add_user = (data) => {};
+// CREATE USER
+// GET USERS
+// GET USER BY ID
+// UPDATE USER
+// DELETE USER
+// Search user
