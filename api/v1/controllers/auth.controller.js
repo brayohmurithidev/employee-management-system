62dc215e7edb24982755e85678b3dd8d555ae038
brayohmurithidev <murithibrianm@gmail.com>
2023-10-27 01:34:55 +0300

    added login function

--- api/v1/controllers/auth.controller.js
+++ api/v1/controllers/auth.controller.js
@@ -1,6 +1,10 @@
 import { User } from "../models/index.models.js";
 import { apiResponse } from "../utils/response.js";
-import { hash_password } from "../utils/utils.js";
+import {
+  confirm_password,
+  generate_token,
+  hash_password,
+} from "../utils/utils.js";
 
 // Register User
 export const register_user = async (req, res, next) => {
@@ -15,8 +19,8 @@ export const register_user = async (req, res, next) => {
             "BAD_PASSWORD_LENGTH",
             null,
             "Password must be at least 8 characters long",
-            400
-          )
+            400,
+          ),
         );
     } else {
       password = hash_password(password);
@@ -31,8 +35,8 @@ export const register_user = async (req, res, next) => {
             "CONFLICT",
             null,
             `User with email ${email} already exists`,
-            400
-          )
+            400,
+          ),
         );
     }
     await User.create({
@@ -40,7 +44,7 @@ export const register_user = async (req, res, next) => {
       password: password,
       userRoles: roles,
     });
-    return response
+    return res
       .status(200)
       .json(apiResponse("SUCCESS", null, "User created successfully", 201));
   } catch (error) {
@@ -49,6 +53,40 @@ export const register_user = async (req, res, next) => {
 };
 
 // LOGIN USER
+
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    let session;
+
+    const user = await User.findOne({ where: { userEmail: email } });
+    if (!user) {
+      return res
+        .status(404)
+        .json(
+          apiResponse("NOT_FOUND", null, `User with email ${email} not found`),
+        );
+    }
+    const check_pwd = await confirm_password(password, user.password);
+    if (!check_pwd) {
+      return res
+        .status(401)
+        .json({ status: "UNAUTHORIZED", msg: `Password is incorrect` });
+    }
+    const data = {
+      id: user.id,
+      roles: user.userRoles,
+    };
+    const token = await generate_token(data);
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 60 * 60,
+    });
+    return res.sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
+};
 
 // 2FA LOGIN
 // REFRESH USER TOKEN
