d898d0d7b0aff8e56f213373879e552536e62c17
brayohmurithidev <murithibrianm@gmail.com>
2023-10-28 23:42:02 +0300

    added reset password route

--- api/v1/routes/auth.route.js
+++ api/v1/routes/auth.route.js
@@ -3,6 +3,7 @@ import {
   login,
   logout,
   refresh_token,
+  reset_password,
 } from "../controllers/auth.controller.js";
 import { verify_token } from "../middlewares/auth.middleware.js";
 
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router.route("/").post(login);
 router.route("/refresh-token").post(refresh_token);
+router.route("/reset-password/:id").post(verify_token, reset_password);
 router.route("/logout").post(verify_token, logout);
 
 export default router;
