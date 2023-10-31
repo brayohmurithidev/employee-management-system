34ee1933db35973ff17e5e6df90fb728a86ad3c3
brayohmurithidev <murithibrianm@gmail.com>
2023-10-31 15:09:11 +0300

    reset otp mail template

--- api/v1/utils/mailTemplates.js
+++ api/v1/utils/mailTemplates.js
@@ -8,3 +8,14 @@ export const reset_password_template = (name, userEmail, password) => {
   <h4>Password: <span style="font-size: 10px">${password}</span></h4>
   `;
 };
+
+export const reset_password_otp = (otp, email) => {
+  return `
+  <h2 style="color: green">Hello</h2>
+  <p>You can reset your password by clicking the link below, or using the code</p>
+  <h4> <a href=\`http://localhost:3000/login/?otp=${otp}&email=${email}\`>RESET</a> </h4>
+  <h4 style="font-size: 30px">CODE: <span >${otp}</span> </h4> 
+  
+  <h3>NB: The link and code expires in 1 hour</h3>
+  `;
+};
