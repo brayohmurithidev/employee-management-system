66955ebfa4ca61466c863cda659dbfed3b477039
brayohmurithidev <murithibrianm@gmail.com>
2023-10-31 15:02:04 +0300

    reset password funcs

--- api/v1/routes/auth.route.js
+++ api/v1/routes/auth.route.js
@@ -3,7 +3,9 @@ import {
   login,
   logout,
   refresh_token,
+  request_otp,
   reset_password,
+  reset_password_by_otp,
 } from "../controllers/auth.controller.js";
 import { verify_token } from "../middlewares/auth.middleware.js";
 
@@ -11,6 +13,8 @@ const router = express.Router();
 
 router.route("/").post(login);
 router.route("/refresh-token").post(refresh_token);
+router.route("/request-otp").post(request_otp);
+router.route("/reset-password-with-otp").post(reset_password_by_otp);
 router.route("/reset-password/:id").post(verify_token, reset_password);
 router.route("/logout").post(verify_token, logout);
 
