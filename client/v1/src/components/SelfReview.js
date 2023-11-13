8c2a6f97a3d9989feb99c63414bedf9bf717143c
brayohmurithidev <murithibrianm@gmail.com>
2023-11-13 20:37:40 +0300

    added review component

---
+++ client/v1/src/components/SelfReview.js
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Modal,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { Visibility } from "@mui/icons-material";
+import NewAppraisal from "./NewAppraisal";
+
+const SelfReview = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <>
+      <Paper elevation={3} sx={{ width: "100%", padding: "20px" }}>
+        {/*  APPLY LEAVE BUTTON*/}
+        <Button
+          onClick={handleOpen}
+          sx={{ marginBottom: "20px" }}
+          variant="contained"
+        >
+          SELF APPRAISAL
+        </Button>
+
+        {/*  LEAVE DETAILS*/}
+
+        <Paper sx={{ width: "100%", overflow: "hidden" }}>
+          <TableContainer sx={{ maxHeight: 440, width: "100%" }}>
+            <Table stickyHeader>
+              <TableHead>
+                <TableRow>
+                  <TableCell>#</TableCell>
+                  <TableCell>Review Date</TableCell>
+                  <TableCell>Appraisal Type</TableCell>
+                  <TableCell>Status</TableCell>
+                  <TableCell>Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>1</TableCell>
+                  <TableCell>2023-11-14</TableCell>
+                  <TableCell>Annual</TableCell>
+                  <TableCell>Completed</TableCell>
+                  <TableCell>
+                    <Button startIcon={<Visibility />}>VIEW</Button>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>1</TableCell>
+                  <TableCell>2023-11-14</TableCell>
+                  <TableCell>Annual</TableCell>
+                  <TableCell>Completed</TableCell>
+                  <TableCell>
+                    <Button startIcon={<Visibility />}>VIEW</Button>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>2</TableCell>
+                  <TableCell>2023-11-14</TableCell>
+                  <TableCell>Annual</TableCell>
+                  <TableCell>Completed</TableCell>
+                  <TableCell>
+                    <Button startIcon={<Visibility />}>VIEW</Button>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>3</TableCell>
+                  <TableCell>2023-11-14</TableCell>
+                  <TableCell>Annual</TableCell>
+                  <TableCell>Completed</TableCell>
+                  <TableCell>
+                    <Button startIcon={<Visibility />}>VIEW</Button>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>4</TableCell>
+                  <TableCell>2023-11-14</TableCell>
+                  <TableCell>Annual</TableCell>
+                  <TableCell>Completed</TableCell>
+                  <TableCell>
+                    <Button startIcon={<Visibility />}>VIEW</Button>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
+      </Paper>
+
+      <Modal open={open} onClose={handleClose}>
+        <Paper
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: "content-width",
+            backgroundColor: "background.paper",
+            padding: "20px",
+          }}
+          elevation={3}
+        >
+          <NewAppraisal />
+        </Paper>
+      </Modal>
+    </>
+  );
+};
+
+export default SelfReview;
